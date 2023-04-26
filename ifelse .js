a003a1c71ddfcb2a90f632fee4085dd5eda5c97a
annusingh58 <127593919+annusingh58@users.noreply.github.com>
2023-04-26 11:03:01 -0400

    updates

---
+++ ifelse .js
@@ -0,0 +1,164 @@
+//  Write a program that takes in a temperature in Celsius and converts it to Fahrenheit.
+
+// var Cel = 5;
+// var Far = Cel*9/5+32;
+
+//  console.log(Far);
+  
+
+// convert fahrenheit to celsius question
+
+// var far = 39;
+// var cel = 5/9*(far-32);
+
+// console.log("cel",cel);
+
+
+
+//  Write a program that takes in two integers and prints out their sum, difference, product, and quotient.
+
+// var num1 =10;
+// var num2 =20;
+// var add =num1+num2;
+// var sub =num1-num2;
+// var product=num1*num2;
+// var quotient=num1%num2
+
+// console.log(add);
+// console.log(sub);
+// console.log(product);
+// console.log(quotient);
+
+
+
+
+// Write a program that takes in an integer and checks if it's even or odd.
+
+// var num = 21;
+// if(num%2==0){                      
+//     console.log("no is even");
+
+// }
+// else{
+//     console.log("no is odd");
+// }
+
+
+
+// Write a program that takes in a character and checks if it's a letter or a digit.
+
+// var a = '9';
+
+//  if(a<=9 && a>=0){
+//     console.log("digit");
+//  }
+// else if(a>='a' && a<='z'  ){
+//     console.log("letter");
+//  }
+
+// else{
+//     console.log("not defined")
+// }
+
+
+
+// Write a program that takes in a character and checks if it's an uppercase letter, lowercase letter, or neither.
+// var a='@';
+
+// if(a>='a' && a<='z' ){ 
+//     console.log("lowercase");
+// }
+// else if(a>="A" && a<="Z"){
+//     console.log("uppercase");
+// }
+// else{
+//     console.log("not defined");
+// }
+
+
+
+
+// Write a program that takes in an integer and checks if it's a multiple of 3 or 5.
+
+// var num= 255;
+// if(num%3==0 || num%5==0){
+//     console.log("multiple of 3 and 5");
+// }
+// else{
+//     console.log("not defined")
+// }
+
+
+
+// write a program that takes in two integers and checks if their sum is even
+
+// var num1=15;
+// var num2=10;
+// if((num1+num2)%2==0){
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// }
+
+
+
+// Write a program that takes in a year and checks if it's a leap year or not.
+// var year=1900;
+// if(year%4==0 && (year%100!=0 || year%400==0 )){
+//     console.log("leap year");
+// }
+// else{
+//     console.log("not a leap year");
+// }
+
+
+
+
+ // Write a program that takes in two integers and checks if their sum is greater than 100.
+// var num1=25;
+// var num2=1;
+// var sum =num1+num2;
+// if(sum>100){
+//     console.log("no is greater then 100");
+// }
+// else if (sum==100){
+//     console.log("equal to 100");
+// }
+// else{
+//     console.log("less then 100");
+// }
+
+// Write a program that takes in three integers and finds the largest and smallest among them.
+//   var num1=10;
+//   var num2=5;
+//   var num3=80;
+//   if((num1>num2 && num1>num3) && (num2<num3<num1) ){
+//         console.log(num1," is largest",num2,"is smallest");
+//   }
+//   else if((num2>num1 && num2>num3) && (num3<num1<num2)){
+//     console.log(num2,"is largest",num3 ," is smallest");
+
+//   }
+//  else if((num3>num1 && num3>num2) && (num1<num2<num3)){
+//     console.log(num3,"is largest",num1,"is smallest");
+//   }
+//   else {
+//     console.log("invalid");
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
